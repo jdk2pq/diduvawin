9be786523d95653c44c4968154299a8647d5ec61
Jake Kenneally <jakekenneally@gmail.com>
2020-03-16 22:15:46 -0400

    fix for when the season ends and there's no mostRecentGame

--- src/service.ts
+++ src/service.ts
@@ -24,7 +24,7 @@ import {
 export const didWeWin = (req: Request, res: Response) => {
     request(getScheduleUrl('football'), (footballError, footballResponse, footballBody: string) => {
         request(getScheduleUrl('basketball'), (basketballError, basketballResponse, basketballBody: string) => {
-            let status = 'NOT YET';
+            let status ='NOT YET';
             let score: number = 0;
             let competitorScore: number = 0;
             let competitorObject: IESPNCompetitorsJSON;
@@ -40,67 +40,71 @@ export const didWeWin = (req: Request, res: Response) => {
             const mostRecentEvent: IESPNPastEvent = getMostRecentEvent(nonCanceledEvents);
             const nextEventStr = getNextEvent(nonCanceledEvents);
 
-            if (mostRecentEvent.competitions[0].status.type.completed) {
-                const winner = mostRecentEvent.competitions[0].competitors.find(competitor => competitor.winner);
-                // The winner may not be available yet if the game completed within the last few minutes
-                if (winner) {
-                    if (winner.id === ESPN_TEAM_ID) {
-                        status = 'YES';
-                    } else {
-                        status = 'NO';
+            if (mostRecentEvent) {
+                if (mostRecentEvent.competitions[0].status.type.completed) {
+                    const winner = mostRecentEvent.competitions[0].competitors.find(competitor => competitor.winner);
+                    // The winner may not be available yet if the game completed within the last few minutes
+                    if (winner) {
+                        if (winner.id === ESPN_TEAM_ID) {
+                            status = 'YES';
+                        } else {
+                            status = 'NO';
+                        }
                     }
                 }
-            }
 
-            let us:
-                | IESPNPastCompetitorsJSON
-                | IESPNCurrentCompetitorsJSON = mostRecentEvent.competitions[0].competitors.find(
-                competitor => competitor.id === ESPN_TEAM_ID
-            );
-
-            const summaryLinkFound = mostRecentEvent.links.find(
-                link =>
-                    (link.rel.includes('now') ||
-                        link.rel.includes('recap') ||
-                        link.rel.includes('live') ||
-                        link.rel.includes('summary')) &&
-                    link.rel.includes('desktop')
-            );
-            if (summaryLinkFound) {
-                summaryLink = summaryLinkFound.href;
-            }
-            competitorObject = mostRecentEvent.competitions[0].competitors.find(
-                competitor => competitor.id !== ESPN_TEAM_ID
-            );
-            competitor = competitorObject.team.abbreviation;
-
-            if (us.score) {
-                // Game is complete
-                score = us.score.value;
-                competitorScore = competitorObject.score.value;
-                render(res, summaryLink, status, score, competitor, competitorScore, nextEventStr);
-            } else {
-                // Game is currently happening, so we need to make a separate request since the schedule doesn't have current scores
-                request(
-                    getCurrentGameUrl(
-                        summaryLink.includes('basketball') ? 'basketball' : 'football',
-                        mostRecentEvent.id
-                    ),
-                    (err, response, body: string) => {
-                        let time;
-                        if (!err && response.statusCode === 200) {
-                            const event: IESPNCurrentEvent = JSON.parse(body);
-                            us = event.competitions[0].competitors.find(competitor => competitor.id === ESPN_TEAM_ID);
-                            score = +us.score;
-                            competitorObject = event.competitions[0].competitors.find(
-                                competitor => competitor.id !== ESPN_TEAM_ID
-                            );
-                            competitorScore = +competitorObject.score;
-                            time = event.status.type.shortDetail;
-                        }
-                        render(res, summaryLink, status, score, competitor, competitorScore, nextEventStr, time);
-                    }
+                let us:
+                    | IESPNPastCompetitorsJSON
+                    | IESPNCurrentCompetitorsJSON = mostRecentEvent.competitions[0].competitors.find(
+                    competitor => competitor.id === ESPN_TEAM_ID
                 );
+
+                const summaryLinkFound = mostRecentEvent.links.find(
+                    link =>
+                        (link.rel.includes('now') ||
+                            link.rel.includes('recap') ||
+                            link.rel.includes('live') ||
+                            link.rel.includes('summary')) &&
+                        link.rel.includes('desktop')
+                );
+                if (summaryLinkFound) {
+                    summaryLink = summaryLinkFound.href;
+                }
+                competitorObject = mostRecentEvent.competitions[0].competitors.find(
+                    competitor => competitor.id !== ESPN_TEAM_ID
+                );
+                competitor = competitorObject.team.abbreviation;
+
+                if (us.score) {
+                    // Game is complete
+                    score = us.score.value;
+                    competitorScore = competitorObject.score.value;
+                    render(res, summaryLink, status, score, competitor, competitorScore, nextEventStr);
+                } else {
+                    // Game is currently happening, so we need to make a separate request since the schedule doesn't have current scores
+                    request(
+                        getCurrentGameUrl(
+                            summaryLink.includes('basketball') ? 'basketball' : 'football',
+                            mostRecentEvent.id
+                        ),
+                        (err, response, body: string) => {
+                            let time;
+                            if (!err && response.statusCode === 200) {
+                                const event: IESPNCurrentEvent = JSON.parse(body);
+                                us = event.competitions[0].competitors.find(competitor => competitor.id === ESPN_TEAM_ID);
+                                score = +us.score;
+                                competitorObject = event.competitions[0].competitors.find(
+                                    competitor => competitor.id !== ESPN_TEAM_ID
+                                );
+                                competitorScore = +competitorObject.score;
+                                time = event.status.type.shortDetail;
+                            }
+                            render(res, summaryLink, status, score, competitor, competitorScore, nextEventStr, time);
+                        }
+                    );
+                }
+            } else {
+                render(res, summaryLink, status, score, competitor, competitorScore, nextEventStr);
             }
         });
     });
