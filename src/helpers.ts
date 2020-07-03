b98af91a13433e2380da88428856557371cc809c
Jake Kenneally <jakekenneally@gmail.com>
2020-07-03 11:21:43 -0400

    more changes on the API side, so cleaning things up and adding a new hopeful message

--- src/helpers.ts
+++ src/helpers.ts
@@ -13,7 +13,7 @@ export const getMostRecentEvent = (events: Array<IESPNPastEvent>): IESPNPastEven
         events.filter((event: IESPNPastEvent) => {
             return moment(event.date).isBefore(today);
         }),
-        game => moment(game.date).valueOf()
+        (game) => moment(game.date).valueOf()
     );
 };
 
@@ -26,17 +26,21 @@ export const getNextEvent = (events: Array<IESPNPastEvent>): string => {
         events.filter((event: IESPNPastEvent) => {
             return moment(event.date).isAfter(today);
         }),
-        game => moment(game.date).valueOf()
+        (game) => moment(game.date).valueOf()
     );
-    const nextDate = moment(nextEvent.date);
-    let date = nextDate.tz('America/New_York').format('M/DD');
-    if (today.format('M/DD') === date) {
-        date = `today at ${nextDate.tz('America/New_York').format('h:mma z')}`;
+    if (nextEvent) {
+        const nextDate = moment(nextEvent.date);
+        let date = nextDate.tz('America/New_York').format('M/DD');
+        if (today.format('M/DD') === date) {
+            date = `today at ${nextDate.tz('America/New_York').format('h:mma z')}`;
+        } else {
+            date = `on ${date}`;
+        }
+        const sport = nextEvent.competitions[0].competitors[0].team.links[0].href.includes('basketball')
+            ? "Men's Basketball"
+            : 'Football';
+        return `${sport} ▸ ${nextEvent.shortName} ${date}`;
     } else {
-        date = `on ${date}`;
+        return '';
     }
-    const sport = nextEvent.competitions[0].competitors[0].team.links[0].href.includes('basketball')
-        ? "Men's Basketball"
-        : 'Football';
-    return `${sport} ▸ ${nextEvent.shortName} ${date}`;
 };
--- src/service.ts
+++ src/service.ts
@@ -24,7 +24,7 @@ import {
 export const didWeWin = (req: Request, res: Response) => {
     request(getScheduleUrl('football'), (footballError, footballResponse, footballBody: string) => {
         request(getScheduleUrl('basketball'), (basketballError, basketballResponse, basketballBody: string) => {
-            let status ='NOT YET';
+            let status = 'HOPEFULLY.<br>STAY SAFE!';
             let score: number = 0;
             let competitorScore: number = 0;
             let competitorObject: IESPNCompetitorsJSON;
@@ -35,14 +35,14 @@ export const didWeWin = (req: Request, res: Response) => {
             const basketballSchedule: IESPNSchedule = JSON.parse(basketballBody);
             const concatenatedEvents = footballSchedule.events.concat(basketballSchedule.events);
             const nonCanceledEvents = concatenatedEvents.filter(
-                e => e.competitions[0].status.type.name !== STATUSES.CANCELED
+                (e) => e.competitions[0].status.type.name !== STATUSES.CANCELED
             );
             const mostRecentEvent: IESPNPastEvent = getMostRecentEvent(nonCanceledEvents);
             const nextEventStr = getNextEvent(nonCanceledEvents);
 
             if (mostRecentEvent) {
                 if (mostRecentEvent.competitions[0].status.type.completed) {
-                    const winner = mostRecentEvent.competitions[0].competitors.find(competitor => competitor.winner);
+                    const winner = mostRecentEvent.competitions[0].competitors.find((competitor) => competitor.winner);
                     // The winner may not be available yet if the game completed within the last few minutes
                     if (winner) {
                         if (winner.id === ESPN_TEAM_ID) {
@@ -56,11 +56,11 @@ export const didWeWin = (req: Request, res: Response) => {
                 let us:
                     | IESPNPastCompetitorsJSON
                     | IESPNCurrentCompetitorsJSON = mostRecentEvent.competitions[0].competitors.find(
-                    competitor => competitor.id === ESPN_TEAM_ID
+                    (competitor) => competitor.id === ESPN_TEAM_ID
                 );
 
                 const summaryLinkFound = mostRecentEvent.links.find(
-                    link =>
+                    (link) =>
                         (link.rel.includes('now') ||
                             link.rel.includes('recap') ||
                             link.rel.includes('live') ||
@@ -71,7 +71,7 @@ export const didWeWin = (req: Request, res: Response) => {
                     summaryLink = summaryLinkFound.href;
                 }
                 competitorObject = mostRecentEvent.competitions[0].competitors.find(
-                    competitor => competitor.id !== ESPN_TEAM_ID
+                    (competitor) => competitor.id !== ESPN_TEAM_ID
                 );
                 competitor = competitorObject.team.abbreviation;
 
@@ -81,7 +81,9 @@ export const didWeWin = (req: Request, res: Response) => {
                     competitorScore = competitorObject.score.value;
                     render(res, summaryLink, status, score, competitor, competitorScore, nextEventStr);
                 } else {
-                    // Game is currently happening, so we need to make a separate request since the schedule doesn't have current scores
+                    // Game is currently happening, so we need to make a separate request
+                    // since the schedule doesn't have current scores
+                    status = 'NOT YET';
                     request(
                         getCurrentGameUrl(
                             summaryLink.includes('basketball') ? 'basketball' : 'football',
@@ -91,10 +93,12 @@ export const didWeWin = (req: Request, res: Response) => {
                             let time;
                             if (!err && response.statusCode === 200) {
                                 const event: IESPNCurrentEvent = JSON.parse(body);
-                                us = event.competitions[0].competitors.find(competitor => competitor.id === ESPN_TEAM_ID);
+                                us = event.competitions[0].competitors.find(
+                                    (competitor) => competitor.id === ESPN_TEAM_ID
+                                );
                                 score = +us.score;
                                 competitorObject = event.competitions[0].competitors.find(
-                                    competitor => competitor.id !== ESPN_TEAM_ID
+                                    (competitor) => competitor.id !== ESPN_TEAM_ID
                                 );
                                 competitorScore = +competitorObject.score;
                                 time = event.status.type.shortDetail;
